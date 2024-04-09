68ab3bb4f6f140308aeb14f0aea83f7b1844678a
Ayush-07-Mishra <ayushmishra312004@gmail.com>
2024-04-09 16:18:55 +0530

    Navbar

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,10 @@
 import React from 'react'
+import Navbar from './components/Navbar'
 
 function App() {
   return (
-    <div className='w-full w h-screen bg-zinc-900'>
-      App
+    <div className='w-full w h-screen bg-white'>
+      <Navbar/>
     </div>
   )
 }
