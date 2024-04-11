c34a5b8aa6799ebfeb2cbb6f3fdf5662be4124d9
Ayush-07-Mishra <ayushmishra312004@gmail.com>
2024-04-11 22:44:53 +0530

    About

---
+++ src/components/About.jsx
@@ -0,0 +1,51 @@
+import React from 'react'
+import { FaArrowUpLong } from "react-icons/fa6";
+
+function About() {
+  return (
+    <div className='w-full p-[6vw] bg-[#CDEA68] rounded-tr-3xl rounded-tl-3xl '>
+      <h1 className='Neue Montreal text-[4vw] leading-[3.5vw]'>Ochi is a strategic partner for fast-growing tech businesses that need to raise funds, sell products, explain complex ideas, and hire great people.</h1>
+      <div className='w-full border-t-[2px] border-[#7f8e41] mt-20 flex py-[1.5vw]'>
+      <div className='w-1/2'>
+        <h1>What you can expect:</h1>
+      </div>
+      <div className='w-1/2 flex justify-between'>
+        <div>
+            <div className='w-1/2'>
+                <p>We create tailored presentations to help you persuade your colleagues, clients, or investors. Whether it’s live or digital, delivered for one or a hundred people</p><br/>
+            </div>
+            <div className='w-1/2'>
+                <p>We believe the mix of strategy and design (with a bit of coffee) is what makes your message clear, convincing, and captivating.</p>
+            </div>
+        </div>
+        <div>
+        <h1>S:</h1><br/>
+            <a>instagram</a><br/>
+            <a>facebook</a><br/>
+            <a>Linkedin</a><br/>
+        </div>
+      </div>
+
+      </div>
+    <div className='mt-20vw'>
+    <div className='w-full border-t-[2px] border-[#7f8e41] mt-20  py-[1.5vw] flex justify-between'>
+        <div>
+            <h1 className='text-[4vw]'>Our approach:</h1>
+            <div className='w-[15vw] flex items-center mt-8 px-[2vw] py-[1vw] text-[1.3vw] text-white bg-[#20260b] border-2 border-[#7f8e41] rounded-full gap-8 '>
+            <button className='uppercase'>read more</button> 
+            <span className='rotate-[45deg]'>
+            <FaArrowUpLong />
+            </span> 
+            </div>
+        </div>
+        <div className='h-[35vw] w-1/2 bg-[#98a952] rounded-2xl'>
+            {/* photo */}
+        </div>
+    </div>
+   
+    </div>
+    </div>
+  )
+}
+
+export default About
--- src/components/Marquee.jsx
+++ src/components/Marquee.jsx
@@ -3,12 +3,12 @@ import React from 'react'
 
 function Marquee() {
   return (
-    <div className='w-full h-[50vh] py-28 bg-[#004D43] rounded-tr-2xl rounded-tl-2xl text-white'>
+    <div className='w-full h-[30vw] py-[7vw] bg-[#004D43] rounded-tr-3xl rounded-tl-3xl text-white'>
         <div className='text border-t-2 border-b-2 border-zinc-500 flex gap-10 overflow-hidden whitespace-nowrap'>
-            <motion.h1 initial={{x:0}} animate={{x:"-100%"}} transition={{ease : "linear", repeat: Infinity,duration: 5}} className='text-[25vh] leading-none font-["Founders Grotesk condensed"] font-extrabold uppercase'>we are ochi</motion.h1>
-            <motion.h1 initial={{x:0}} animate={{x:"-100%"}} transition={{ease : "linear", repeat: Infinity,duration: 5}} className='text-[25vh] leading-none font-["Founders Grotesk condensed"] font-extrabold uppercase'>we are ochi</motion.h1>
-            <motion.h1 initial={{x:0}} animate={{x:"-100%"}} transition={{ease : "linear", repeat: Infinity,duration: 5}} className='text-[25vh] leading-none font-["Founders Grotesk condensed"] font-extrabold uppercase'>we are ochi</motion.h1>
-
+            <motion.h1 initial={{x:0}} animate={{x:"-100%"}} transition={{ease : "linear", repeat: Infinity,duration: 10}} className='text-[15vw] leading-none font-["Founders Grotesk condensed"] font-extrabold uppercase'>we are ochi</motion.h1>
+            <motion.h1 initial={{x:0}} animate={{x:"-100%"}} transition={{ease : "linear", repeat: Infinity,duration: 10}} className='text-[15vw] leading-none font-["Founders Grotesk condensed"] font-extrabold uppercase'>we are ochi</motion.h1>
+            <motion.h1 initial={{x:0}} animate={{x:"-100%"}} transition={{ease : "linear", repeat: Infinity,duration: 10}} className='text-[15vw] leading-none font-["Founders Grotesk condensed"] font-extrabold uppercase'>we are ochi</motion.h1>
+            
         </div>
     </div>
   )
