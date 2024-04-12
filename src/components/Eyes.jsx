733716a7b4e41a797147d2e6f069162ca2714177
Ayush-07-Mishra <ayushmishra312004@gmail.com>
2024-04-12 16:59:35 +0530

    new

--- src/components/Eyes.jsx
+++ src/components/Eyes.jsx
@@ -2,7 +2,10 @@ import React from 'react'
 
 function Eyes() {
   return (
-    <div>
+    <div className='eyes w-full h-screen overflow-hidden'>
+        <div className=' relative w-full h-full bg-cover bg-center bg-[url("https://ochi.design/wp-content/uploads/2022/05/Top-Viewbbcbv-1-scaled.jpg")]'>
+            <div className='absoulte top-1/2 left-1/2 -translate-x-[50%] -translate-y-[50%] w-1/2 h-1/3 bg-red-400'></div>
+        </div>
       
     </div>
   )
