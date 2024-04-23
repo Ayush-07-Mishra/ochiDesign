8bdbee3b3f355efa2be2086ac128f78acfef4e22
Ayush-07-Mishra <ayushmishra312004@gmail.com>
2024-04-23 20:49:07 +0530

    eye

--- src/components/Eyes.jsx
+++ src/components/Eyes.jsx
@@ -1,14 +1,24 @@
-import React from 'react'
+import React from "react";
 
 function Eyes() {
   return (
-    <div className='eyes w-full h-screen overflow-hidden'>
-        <div className=' relative w-full h-full bg-cover bg-center bg-[url("https://ochi.design/wp-content/uploads/2022/05/Top-Viewbbcbv-1-scaled.jpg")]'>
-            <div className='absoulte top-1/2 left-1/2 -translate-x-[50%] -translate-y-[50%] w-1/2 h-1/3 bg-red-400'></div>
+    <div className="eyes w-full h-screen overflow-hidden">
+      <div className='relative w-full h-full bg-cover bg-center bg-[url("https://ochi.design/wp-content/uploads/2022/05/Top-Viewbbcbv-1-scaled.jpg")]'>
+        <div className="absolute flex justify-between text-center gap-8 top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2">
+          <div className="flex justify-center items-center w-[15vw] h-[15vw] rounded-full bg-[#F1F1F1]">
+            <div className="flex justify-center items-center w-3/5 h-3/5 bg-zinc-800 rounded-full">
+              <div className="text-[#f1f1f1] uppercase text-[1.2vw] font-light">Play</div>
+            </div>
+          </div>
+          <div className=" flex justify-center items-center w-[15vw] h-[15vw] rounded-full bg-[#F1F1F1]">
+          <div className="flex justify-center items-center w-3/5 h-3/5 bg-zinc-800 rounded-full">
+              <div className="text-[#f1f1f1] uppercase text-[1.2vw] font-light">Play</div>
+            </div>
+          </div>
         </div>
-      
+      </div>
     </div>
-  )
+  );
 }
 
-export default Eyes
+export default Eyes;
